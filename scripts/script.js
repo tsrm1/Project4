59e41627fc22ca6e064e322aeb845b871cedeb01
tsrm1 <4r@ukr.net>
2025-11-18 11:39:55 +0100

    Project4.Initialisation

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+// https://www.figma.com/file/h7ecVRqH6mqrf2JYkCyb0C/To-do-list-(Community)?type=design&node-id=348-221&mode=design
+
+// 1 этап: верстка,
+// 2 этап: добавление задачи, подключение к localstorage,
+// 3 этап: фильтрация, редактирование
+
+// доп. функ: поиск, редактирование задачи(название, время)
+
+// task(entity):{
+//     id:String|Number, // Math.random() | Sate.now()
+//     title: String,
+//     date: String | Number,
+//     completed: Boolean //false->true
+// }
+
+// const tasks=[{
+//     id:1,
+//     title:"Learn React",
+//     data: "12.12.2025",
+//     completed: false,
+// }]
